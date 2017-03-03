13beeaa02fea745935994577b1ea202e765320a8
ryanvalentin <ryan@disqus.com>
2017-03-03 15:52:32 -0800

    Always use https for embed/count scripts

--- disqus/public/js/comment_count.js
+++ disqus/public/js/comment_count.js
@@ -13,6 +13,6 @@ var disqus_shortname = countVars.disqusShortname;
     var s = document.createElement('script');
     s.async = true;
     s.type = 'text/javascript';
-    s.src = '//' + disqus_shortname + '.disqus.com/count.js';
+    s.src = 'https://' + disqus_shortname + '.disqus.com/count.js';
     (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
 }());
