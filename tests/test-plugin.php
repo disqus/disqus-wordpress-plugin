4d3982f76bdada10b461b10b61de01bd595d4a10
ryanvalentin <ryan@disqus.com>
2017-05-15 10:49:17 -0700

    Add admin menu test

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -6,9 +6,20 @@
 class PluginTest extends WP_UnitTestCase {
 
     /**
-     * Check that that activation doesn't break.
+     * Check that activation doesn't break.
      */
     function test_plugin_activated() {
         $this->assertTrue( is_plugin_active( PLUGIN_PATH ) );
     }
+
+    /**
+     * Check that the Disqus link is in the admin menu.
+     */
+     function test_disqus_admin_menu() {
+        global $menu;
+
+        $this->assertTrue( array_key_exists( 24, $menu ) ); // Index 24 is the custom position of Disqus menu page.
+        $this->assertFalse( array_key_exists( 25, $menu ) ); // Index 25 is the default WordPress comments page.
+        $this->assertTrue( $menu[24][0] === 'Disqus' );
+     }
 }
