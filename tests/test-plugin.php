85c31df4b2b11dffc8c366513175efaa2bcb4a0b
ryanvalentin <ryan@disqus.com>
2017-05-15 11:27:46 -0700

    Debug menu test

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -18,8 +18,10 @@ class PluginTest extends WP_UnitTestCase {
      function test_disqus_admin_menu() {
         global $menu;
 
-        $this->assertTrue( array_key_exists( 24, $menu ), 'Disqus not found in $menu global, was ' . serialize( $menu ) ); // Index 24 is the custom position of Disqus menu page.
-        $this->assertFalse( array_key_exists( 25, $menu ), 'Comments found in $menu global, was ' . serialize( $menu ) ); // Index 25 is the default WordPress comments page.
-        $this->assertTrue( $menu[24][0] === 'Disqus', 'Disqus not found as title for menu item 24, was ' . serialize( $menu[24] ) );
+        var_dump($menu);
+
+        $this->assertTrue( array_key_exists( 24, $menu ) ); // Index 24 is the custom position of Disqus menu page.
+        $this->assertFalse( array_key_exists( 25, $menu ) ); // Index 25 is the default WordPress comments page.
+        $this->assertTrue( $menu[24][0] === 'Disqus' );
      }
 }
