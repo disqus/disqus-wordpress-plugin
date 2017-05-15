be9e7e849cddbaa6478fc99631017dcc205786ad
ryanvalentin <ryan@disqus.com>
2017-05-15 11:01:50 -0700

    Update rest endpoint const and use serialize()

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -18,8 +18,8 @@ class PluginTest extends WP_UnitTestCase {
      function test_disqus_admin_menu() {
         global $menu;
 
-        $this->assertTrue( array_key_exists( 24, $menu ), 'Disqus not found in $menu global, was ' . var_dump( $menu ) ); // Index 24 is the custom position of Disqus menu page.
-        $this->assertFalse( array_key_exists( 25, $menu ), 'Comments found in $menu global, was ' . var_dump( $menu ) ); // Index 25 is the default WordPress comments page.
-        $this->assertTrue( $menu[24][0] === 'Disqus', 'Disqus not found as title for menu item 24, was ' . var_dump( $menu[24] ) );
+        $this->assertTrue( array_key_exists( 24, $menu ), 'Disqus not found in $menu global, was ' . serialize( $menu ) ); // Index 24 is the custom position of Disqus menu page.
+        $this->assertFalse( array_key_exists( 25, $menu ), 'Comments found in $menu global, was ' . serialize( $menu ) ); // Index 25 is the default WordPress comments page.
+        $this->assertTrue( $menu[24][0] === 'Disqus', 'Disqus not found as title for menu item 24, was ' . serialize( $menu[24] ) );
      }
 }
