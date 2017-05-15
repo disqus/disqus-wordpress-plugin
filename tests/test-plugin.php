00b5e8e887c675d67b09ff7de1ee6193a212e6cd
ryanvalentin <ryan@disqus.com>
2017-05-15 13:51:15 -0700

    Remove admin menu test
    We’ll test the admin function specifically

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -11,20 +11,4 @@ class PluginTest extends WP_UnitTestCase {
     function test_plugin_activated() {
         $this->assertTrue( is_plugin_active( PLUGIN_PATH ) );
     }
-
-    /**
-     * Check that the Disqus link is in the admin menu.
-     */
-     function test_disqus_admin_menu() {
-        $user_id = $this->factory->user->create( array( 'role' => 'administrator' ) );
-        wp_set_current_user( $user_id );
-
-        global $menu;
-
-        var_dump($menu);
-
-        $this->assertTrue( array_key_exists( 24, $menu ) ); // Index 24 is the custom position of Disqus menu page.
-        $this->assertFalse( array_key_exists( 25, $menu ) ); // Index 25 is the default WordPress comments page.
-        $this->assertTrue( $menu[24][0] === 'Disqus' );
-     }
 }
