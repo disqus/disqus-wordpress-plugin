4608d01cb319ef57df6c9838f632a8d26aa00511
ryanvalentin <ryan@disqus.com>
2017-05-15 14:15:04 -0700

    Stub admin tests

---
+++ tests/test-admin.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Admin plugin tests.
+ */
+
+class Test_Admin extends WP_UnitTestCase {
+
+    /**
+     * Check that the admin adds Disqus to the menu.
+     */
+    function test_admin_construct_admin_menu() {
+        $this->assertTrue( true );
+    }
+
+    /**
+     * Check that the admin adds Disqus to the admin bar.
+     */
+    function test_admin_construct_admin_bar() {
+        $this->assertTrue( true );
+    }
+}
