1ce00cc562b528ed3b337137eaa745e70202e9d4
ryanvalentin <ryan@disqus.com>
2017-05-19 15:47:34 -0700

    Start implementing proper sync endpoint with verification

--- disqus/rest-api/class-disqus-rest-api.php
+++ disqus/rest-api/class-disqus-rest-api.php
@@ -97,14 +97,6 @@ class Disqus_Rest_Api {
 		register_rest_route( Disqus_Rest_Api::REST_NAMESPACE, 'comments/sync', array(
 			'methods' => 'POST',
 			'callback' => array( $this, 'rest_comments_sync' ),
-			'args' => array(
-				'post_id' => array(
-					'validate_callback' => function( $param, $request, $key ) {
-						return is_numeric( $param );
-					},
-					'required' => true,
-				),
-			),
 		) );
 
 		register_rest_route( Disqus_Rest_Api::REST_NAMESPACE, 'settings', array(
@@ -181,143 +173,33 @@ class Disqus_Rest_Api {
 	 * it to the local comments database.
 	 *
 	 * @since    3.0
-	 * @param    array $data         The request POST data.
-	 * @return   WP_REST_Response    The API response object.
+	 * @param    array $data         		  The request POST data.
+	 * @return   WP_REST_Response|WP_Error    The API response object.
 	 */
-	public function rest_comments_sync( array $data ) {
-		$dsq_post_id = $data['post_id'];
-		$secret_key = get_option( 'disqus_secret_key' );
-		$access_token = get_option( 'disqus_admin_access_token' );
+	public function rest_comments_sync( WP_REST_Request $request ) {
+		$json_data = $request->get_json_params();
 
-		if ( ! $secret_key ) {
-			return $this->rest_get_error( 'Secret key is not set.' );
+		if ( ! property_exists( 'verb', $json_data ) ) {
+			return new WP_Error( 400, 'Missing required property: verb.' );
 		}
 
-		$api_url = Disqus_Rest_Api::DISQUS_API_BASE . 'posts/details.json?'
-			. 'api_secret=' . $secret_key
-			. '&access_token=' . $access_token
-			. '&post=' . (string) $dsq_post_id
-			. '&related=thread';
-
-		$dsq_response = wp_remote_get( $api_url, array(
-			'headers' => array(
-				'Referer' => '', // Unset referer so we can use secret key.
-			),
-		) );
-
-		if ( ! is_array( $dsq_response ) ) {
-			return $this->rest_get_error( 'Unknown error requesting the Disqus API.' );
-		}
-
-		$dsq_response_data = json_decode( $dsq_response['body'] );
-
-		if ( 0 !== $dsq_response_data->code ) {
-			return $this->rest_get_error( $dsq_response_data->response, array(
-				// TODO: Log these errors somewhere and provide a method in the admin to retry/dismiss.
-			) );
-		}
-
-		$post = $dsq_response_data->response;
-
-		if ( $this->shortname !== $post->forum ) {
-			return $this->rest_get_error( 'The comment\'s forum does not match the installed forum.' );
-		}
-
-		// Check to make sure we haven't synced this comment yet.
-		$comment_query = new WP_Comment_Query( array(
-			'meta_key' => 'dsq_post_id',
-			'meta_value' => $post->id,
-			'number' => 1,
-		) );
-
-		if ( ! empty( $comment_query->comments ) ) {
-			return $this->rest_get_error( 'This comment has already been synced.' );
-		}
-
-		$wp_post_id = null;
-
-		// Look up posts with the Disqus thread ID meta field.
-		$post_query = new WP_Query( array(
-			'meta_key' => 'dsq_thread_id',
-			'meta_value' => $post->thread->id,
-		) );
-
-		if ( $post_query->have_posts() ) {
-			$wp_post_id = $post_query->the_post()->ID;
-			wp_reset_postdata();
-		}
-
-		// If that doesn't exist, get the  and update the matching post metadata.
-		if ( null === $wp_post_id || false === $wp_post_id ) {
-			$identifiers = $post->thread->identifiers;
-			$first_identifier = count( $identifiers ) > 0 ? $identifiers[0] : null;
-
-			if ( null !== $first_identifier ) {
-				$wp_post_id = explode( ' ', $first_identifier, 2 )[0];
+		try {
+			switch ( $json_data->verb ) {
+				case 'verify':
+					// The X-Hub-Signature header was already validated, so we only need to return the challenge.
+					return new WP_REST_Response( $challenge, 200 );
+				case 'create':
+					$this->create_comment_from_post( $json_data->reference );
+					return new WP_REST_Response( '', 201 );
+				case 'update':
+					// TODO: Implement updating comment from post.
+					return new WP_REST_Response( '', 200 );
+				default:
+					return new WP_REST_Response( '', 204 );
 			}
-
-			// Keep the post's thread ID meta up to date.
-			update_post_meta( $wp_post_id, 'dsq_thread_id', $post->thread->id );
+		} catch ( Exception $e ) {
+			return new WP_Error( 500, $e->getMessage() );
 		}
-
-		if ( null === $wp_post_id || false == $wp_post_id ) {
-			return $this->rest_get_error( 'No post found associated with the thread.', array(
-				// TODO: Log these errors somewhere and provide a method in the admin to retry/dismiss.
-			) );
-		}
-
-		$parent = 0;
-		if ( null !== $post->parent ) {
-			$parent_comment_query = new WP_Comment_Query( array(
-				'meta_key' => 'dsq_post_id',
-				'meta_value' => (string) $post->parent,
-				'number' => 1,
-			) );
-
-			if ( empty( $comment_query->comments ) ) {
-				return $this->rest_get_error( 'This comment\'s parent has not been synced yet.', array(
-					// TODO: Log these errors somewhere and provide a method in the admin to retry/dismiss.
-				) );
-			} else {
-				$parent = $comment_query->comments[0]->$comment_ID;
-			}
-		}
-
-		// Email is a special permission for Disqus API applications and won't be present
-		// if the user has not set the permission for their API application.
-		$author_email = null;
-		if ( isset( $post->author->email ) ) {
-			$author_email = $post->author->email;
-		} elseif ( $post->author->isAnonymous ) {
-			$author_email = 'anonymized-' . md5( $post->author->name ) . '@disqus.com';
-		} else {
-			$author_email = 'user-' . $post->author->id . '@disqus.com';
-		}
-
-		$wp_request = new WP_REST_Request( 'POST', '/wp/v2/comments' );
-		$wp_request->set_param( 'author_email', $author_email );
-		$wp_request->set_param( 'author_name', $post->author->name );
-		$wp_request->set_param( 'author_url', $post->author->url );
-		// you can also set ( 'date_gmt', $post->createdAt ).
-		$wp_request->set_param( 'content', $post->raw_message );
-		$wp_request->set_param( 'post', (int) $wp_post_id );
-		$wp_request->set_param( 'parent', $parent );
-
-		$wp_response = rest_do_request( $wp_request );
-
-		if ( $wp_response->is_error() ) {
-			$wp_error = $wp_response->as_error();
-			return $this->rest_get_error( $wp_error->get_error_message(), array(
-				// TODO: Log these errors somewhere and provide a method in the admin to retry/dismiss.
-			) );
-		}
-
-		$wp_response_data = $wp_response->get_data();
-
-		// Add Disqus post ID as meta to local comment to avoid duplicates.
-		add_comment_meta( $wp_response_data['id'], 'dsq_post_id', $post->id );
-
-		return $this->rest_get_response( $wp_response_data );
 	}
 
 	/**
@@ -325,8 +207,8 @@ class Disqus_Rest_Api {
 	 * it to the local comments database.
 	 *
 	 * @since    3.0
-	 * @param    WP_REST_Request $request    The request object.
-	 * @return   WP_REST_Response     		 The API response object.
+	 * @param    WP_REST_Request $request     The request object.
+	 * @return   WP_REST_Response|WP_Error    The API response object.
 	 */
 	public function rest_settings( WP_REST_Request $request ) {
 		$should_update = 'POST' === $request->get_method();
@@ -340,8 +222,8 @@ class Disqus_Rest_Api {
 	 * Endpoint callback for fetching automatic syncing status.
 	 *
 	 * @since    3.0
-	 * @param    WP_REST_Request $request    The request object.
-	 * @return   WP_REST_Response     		 The API response object.
+	 * @param    WP_REST_Request $request     The request object.
+	 * @return   WP_REST_Response|WP_Error    The API response object.
 	 */
 	public function rest_sync_status( WP_REST_Request $request ) {
 		try {
@@ -357,8 +239,8 @@ class Disqus_Rest_Api {
 	 * Endpoint callback for enabling automatic syncing.
 	 *
 	 * @since    3.0
-	 * @param    WP_REST_Request $request    The request object.
-	 * @return   WP_REST_Response     		 The API response object.
+	 * @param    WP_REST_Request $request     The request object.
+	 * @return   WP_REST_Response|WP_Error    The API response object.
 	 */
 	public function rest_sync_enable( WP_REST_Request $request ) {
 		try {
@@ -374,8 +256,8 @@ class Disqus_Rest_Api {
 	 * Endpoint callback for disabling automatic syncing.
 	 *
 	 * @since    3.0
-	 * @param    WP_REST_Request $request    The request object.
-	 * @return   WP_REST_Response     		 The API response object.
+	 * @param    WP_REST_Request $request     The request object.
+	 * @return   WP_REST_Response|WP_Error    The API response object.
 	 */
 	public function rest_sync_disable( WP_REST_Request $request ) {
 		try {
@@ -562,6 +444,107 @@ class Disqus_Rest_Api {
 		}
 
 		return $sync_status;
+	}
+
+	/**
+	 * Creates a comment in the WordPress database given a Disqus post.
+	 *
+	 * @since    3.0
+	 * @param    array $post    			  The Disqus post object.
+	 * @throws   Exception    				  An exception if comment can't be saved from post data.
+	 */
+	private function create_comment_from_post( $post ) {
+		if ( $this->shortname !== $post->forum ) {
+			throw new Exception( 'The comment\'s forum does not match the installed forum.' );
+		}
+
+		// Check to make sure we haven't synced this comment yet.
+		$comment_query = new WP_Comment_Query( array(
+			'meta_key' => 'dsq_post_id',
+			'meta_value' => $post->id,
+			'number' => 1,
+		) );
+
+		if ( ! empty( $comment_query->comments ) ) {
+			throw new Exception( 'This comment has already been synced.' );
+		}
+
+		$wp_post_id = null;
+
+		// Look up posts with the Disqus thread ID meta field.
+		$post_query = new WP_Query( array(
+			'meta_key' => 'dsq_thread_id',
+			'meta_value' => $post->thread->id,
+		) );
+
+		if ( $post_query->have_posts() ) {
+			$wp_post_id = $post_query->the_post()->ID;
+			wp_reset_postdata();
+		}
+
+		// If that doesn't exist, get the  and update the matching post metadata.
+		if ( null === $wp_post_id || false === $wp_post_id ) {
+			$identifiers = $post->thread->identifiers;
+			$first_identifier = count( $identifiers ) > 0 ? $identifiers[0] : null;
+
+			if ( null !== $first_identifier ) {
+				$wp_post_id = explode( ' ', $first_identifier, 2 )[0];
+			}
+
+			// Keep the post's thread ID meta up to date.
+			update_post_meta( $wp_post_id, 'dsq_thread_id', $post->thread->id );
+		}
+
+		if ( null === $wp_post_id || false == $wp_post_id ) {
+			throw new Exception( 'No post found associated with the thread.' );
+		}
+
+		$parent = 0;
+		if ( null !== $post->parent ) {
+			$parent_comment_query = new WP_Comment_Query( array(
+				'meta_key' => 'dsq_post_id',
+				'meta_value' => (string) $post->parent,
+				'number' => 1,
+			) );
+
+			if ( empty( $comment_query->comments ) ) {
+				throw new Exception( 'This comment\'s parent has not been synced yet.' );
+			} else {
+				$parent = $comment_query->comments[0]->$comment_ID;
+			}
+		}
+
+		// Email is a special permission for Disqus API applications and won't be present
+		// if the user has not set the permission for their API application.
+		$author_email = null;
+		if ( isset( $post->author->email ) ) {
+			$author_email = $post->author->email;
+		} elseif ( $post->author->isAnonymous ) {
+			$author_email = 'anonymized-' . md5( $post->author->name ) . '@disqus.com';
+		} else {
+			$author_email = 'user-' . $post->author->id . '@disqus.com';
+		}
+
+		$wp_request = new WP_REST_Request( 'POST', '/wp/v2/comments' );
+		$wp_request->set_param( 'author_email', $author_email );
+		$wp_request->set_param( 'author_name', $post->author->name );
+		$wp_request->set_param( 'author_url', $post->author->url );
+		// you can also set ( 'date_gmt', $post->createdAt ).
+		$wp_request->set_param( 'content', $post->raw_message );
+		$wp_request->set_param( 'post', (int) $wp_post_id );
+		$wp_request->set_param( 'parent', $parent );
+
+		$wp_response = rest_do_request( $wp_request );
+
+		if ( $wp_response->is_error() ) {
+			$wp_error = $wp_response->as_error();
+			throw new Exception( $wp_error->get_error_message() );
+		}
+
+		$wp_response_data = $wp_response->get_data();
+
+		// Add Disqus post ID as meta to local comment to avoid duplicates.
+		add_comment_meta( $wp_response_data['id'], 'dsq_post_id', $post->id );
 	}
 
 	/**
