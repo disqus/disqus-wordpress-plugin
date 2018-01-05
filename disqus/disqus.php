dffca0a769763f0931a8a1febb0551d60df9bd6d
ryanvalentin <ryan@disqus.com>
2018-01-05 15:15:46 -0800

    Bump version to 3.0.4-beta

--- disqus/disqus.php
+++ disqus/disqus.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Disqus for WordPress
  * Plugin URI:        https://disqus.com/
  * Description:       Disqus helps publishers increase engagement and build loyal audiences. Supports syncing comments to your database for easy backup.
- * Version:           3.0.3
+ * Version:           3.0.4
  * Author:            Disqus
  * Author URI:        https://disqus.com/
  * License:           GPL-2.0+
@@ -67,7 +67,7 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-disqus.php';
  */
 function run_disqus() {
 
-	$plugin = new Disqus( '3.0.3-beta' );
+	$plugin = new Disqus( '3.0.4-beta' );
 	$plugin->run();
 
 }
