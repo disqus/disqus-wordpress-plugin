f555bd54144e37b2ce38c2fe47311e982d76927c
ryanvalentin <ryan@disqus.com>
2018-02-05 14:46:18 -0800

    Add production deploy script for testing Travis CI
    This is to test travis CI to make sure the environment operates the same way locally. This won't actually do the SVN deploy since there's an `exit` before that point.

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,4 +1,136 @@
-#!/usr/bin/env bash
+#! /bin/bash
+# Based on https://github.com/GaryJones/wordpress-plugin-git-flow-svn-deploy for instructions and credits.
 
-echo "Deployed! Fake news."
-exit 0
+# Set up some default values. Feel free to change these in your own script
+PLUGINSLUG="disqus"
+CURRENTDIR=`pwd`
+SVNPATH="/tmp/$PLUGINSLUG"
+SVNURL="https://plugins.svn.wordpress.org/disqus-comment-system"
+SVNUSER="disqus"
+PLUGINDIR="$CURRENTDIR/$PLUGINSLUG"
+MAINFILE="$PLUGINSLUG.php"
+
+echo "Slug: $PLUGINSLUG"
+echo "Temp checkout path: $SVNPATH"
+echo "Remote SVN repo: $SVNURL"
+echo "SVN username: $SVNUSER"
+echo "Plugin directory: $PLUGINDIR"
+echo "Main file: $MAINFILE"
+echo
+
+# git config
+GITPATH="$PLUGINDIR/" # this file should be in the base of your git repository
+
+# Let's begin...
+echo ".........................................."
+echo
+echo "Preparing to deploy WordPress plugin"
+echo
+echo ".........................................."
+echo
+
+# Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
+PLUGINVERSION=`grep "Version:" $GITPATH/$MAINFILE | awk -F' ' '{print $NF}' | tr -d '\r'`
+echo "$MAINFILE version: $PLUGINVERSION"
+READMEVERSION=`grep "^Stable tag:" $GITPATH/readme.txt | awk -F' ' '{print $NF}' | tr -d '\r'`
+echo "readme.txt version: $READMEVERSION"
+
+# Check the version in package.json and make sure it's also the same
+PACKAGEVERSION=`cat $CURRENTDIR/package.json | jq -r '.version'`
+echo "package.json version: $PACKAGEVERSION"
+
+if [ "$READMEVERSION" = "trunk" ]; then
+	echo "Version in readme.txt & $MAINFILE don't match, but Stable tag is trunk. Let's proceed..."
+elif [ "$PLUGINVERSION" != "$READMEVERSION" ]; then
+	echo "Version in readme.txt & $MAINFILE don't match. Exiting..."
+	exit 1;
+elif [ "$PACKAGEVERSION" != "$PLUGINVERSION" ]; then
+	echo "Version in package.json & $MAINFILE don't match. Exiting..."
+	exit 1;
+elif [ "$PLUGINVERSION" = "$READMEVERSION" ]; then
+	echo "Versions match in readme.txt, package.json and $MAINFILE. Let's proceed..."
+fi
+
+# Temporary so we don't actually push
+echo "Exit before deploying since this is a test."
+exit 1;
+
+echo "Changing to $GITPATH"
+cd $GITPATH
+
+echo
+echo "Creating local copy of SVN repo trunk ..."
+svn checkout $SVNURL $SVNPATH --depth immediates
+svn update --quiet $SVNPATH/trunk --set-depth infinity
+
+echo "Ignoring GitHub specific files"
+svn propset svn:ignore "README.md
+Thumbs.db
+.github/*
+.git
+.gitattributes
+.gitignore" "$SVNPATH/trunk/"
+
+echo "Exporting the HEAD of master from git to the trunk of SVN"
+git checkout-index -a -f --prefix=$SVNPATH/trunk/
+
+# If submodule exist, recursively check out their indexes
+if [ -f ".gitmodules" ]
+	then
+		echo "Exporting the HEAD of each submodule from git to the trunk of SVN"
+		git submodule init
+		git submodule update
+		git config -f .gitmodules --get-regexp '^submodule\..*\.path$' |
+			while read path_key path
+			do
+				#url_key=$(echo $path_key | sed 's/\.path/.url/')
+				#url=$(git config -f .gitmodules --get "$url_key")
+				#git submodule add $url $path
+				echo "This is the submodule path: $path"
+				echo "The following line is the command to checkout the submodule."
+				echo "git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'"
+				git submodule foreach --recursive 'git checkout-index -a -f --prefix=$SVNPATH/trunk/$path/'
+			done
+fi
+
+# Support for the /assets folder on the .org repo.
+echo "Moving assets"
+# Make the directory if it doesn't already exist
+mkdir -p $SVNPATH/assets/
+mv $SVNPATH/trunk/assets/* $SVNPATH/assets/
+svn add --force $SVNPATH/assets/
+svn delete --force $SVNPATH/trunk/assets
+
+echo "Changing directory to SVN and committing to trunk"
+cd $SVNPATH/trunk/
+# Delete all files that should not now be added.
+svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2"@"}' | xargs svn del
+# Add all new files that are not set to be ignored
+svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2"@"}' | xargs svn add
+svn commit --username=$SVNUSER -m "Preparing for $PLUGINVERSION release"
+
+echo "Updating WordPress plugin repo assets and committing"
+cd $SVNPATH/assets/
+# Delete all new files that are not set to be ignored
+svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2"@"}' | xargs svn del
+# Add all new files that are not set to be ignored
+svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2"@"}' | xargs svn add
+svn update --accept mine-full $SVNPATH/assets/*
+svn commit --username=$SVNUSER -m "Updating assets"
+
+echo "Creating new SVN tag and committing it"
+cd $SVNPATH
+svn update --quiet $SVNPATH/tags/$PLUGINVERSION
+svn copy --quiet trunk/ tags/$PLUGINVERSION/
+# Remove assets and trunk directories from tag directory
+svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/assets
+svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/trunk
+cd $SVNPATH/tags/$PLUGINVERSION
+svn commit --username=$SVNUSER -m "Tagging version $PLUGINVERSION"
+
+echo "Removing temporary directory $SVNPATH"
+cd $SVNPATH
+cd ..
+rm -fr $SVNPATH/
+
+echo "*** FIN ***"
