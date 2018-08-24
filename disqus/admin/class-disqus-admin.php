d370b6276d89752495bd268d7d18ae6b3914ab48
Derrick Lin <derricklin@Derricks-MacBook-Pro-2.local>
2018-08-24 10:33:54 -0700

    Added a null guard for the dsq_filter_rest_url. Added functionality tests to ensure dsq_filter_rest_url does not error when _SERVER host is not set.

--- disqus/admin/class-disqus-admin.php
+++ disqus/admin/class-disqus-admin.php
@@ -164,7 +164,7 @@ class Disqus_Admin {
             $rest_host .= ':' . $rest_url_parts['port'];
         }
 
-        $current_host = $_SERVER['HTTP_HOST'];
+        $current_host = isset( $_SERVER['HTTP_HOST'] ) ? $_SERVER['HTTP_HOST'] : $rest_host;
 
         if ( $rest_host !== $current_host ) {
             $rest_url = preg_replace( '/' . $rest_host . '/', $current_host, $rest_url, 1 );
