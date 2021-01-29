bf3fed1e2520d061a88286e6099aaad3f810568b
Brett Stevenson <bstevensondev@gmail.com>
2021-01-29 11:27:29 -0800

    Update plugin version to v3.0.20

--- disqus/disqus.php
+++ disqus/disqus.php
@@ -15,7 +15,7 @@
  * Plugin Name:       Disqus for WordPress
  * Plugin URI:        https://disqus.com/
  * Description:       Disqus helps publishers increase engagement and build loyal audiences. Supports syncing comments to your database for easy backup.
- * Version:           3.0.19
+ * Version:           3.0.20
  * Author:            Disqus
  * Author URI:        https://disqus.com/
  * License:           GPL-2.0+
