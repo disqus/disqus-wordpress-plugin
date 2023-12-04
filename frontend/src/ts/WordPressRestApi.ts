069d31e1523ae3dfc963dc0050d7d28613b869d5
Debian <bitnami@ip-172-26-7-254.us-west-2.compute.internal>
2023-12-04 19:53:53 +0000

    sync parent comments in parallel first then sync child comments one by one

--- frontend/src/ts/WordPressRestApi.ts
+++ frontend/src/ts/WordPressRestApi.ts
@@ -121,7 +121,7 @@ export class WordPressRestApi {
                         status: XHR.status,
                         statusText: XHR.statusText
                     });
-                    console.error('Error', XHR.statusText, 'error XHR --> ', XHR);
+                    console.error('Error Status: ', XHR.statusText, 'Full Error XHR: ', XHR);
                 }
             };
             XHR.send(data);
