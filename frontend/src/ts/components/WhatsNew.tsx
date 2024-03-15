94c1c329d0db6717cc4b7fefce77ce6c60859b4f
Debian <bitnami@ip-172-26-7-254.us-west-2.compute.internal>
2024-03-15 15:54:25 +0000

    add more to WhatsNew

--- frontend/src/ts/components/WhatsNew.tsx
+++ frontend/src/ts/components/WhatsNew.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 
-const versionNumber = '3.0.25';
+const latestVersion = '3.0.25';
 const updates = [
     'The Disqus Embed will now render properly on sites using WordPress Block Themes in addition to Classic Themes',
-    'A refreshed and improved UI for the Disqus plugin admin panel'
+    'A refreshed UI for the Disqus plugin admin panel',
+    'Robust improvements to manual and automatic syncing: multiple bug fixes and increased allowable manual syncing data range',
+    'More informative error messaging in the browser console and plugin UI when encountering issues with manual and automatic syncing for easier troubleshooting'
 ]
 
 const WhatsNew: React.FC = () => {
     return (
         <div className={'whats-new'}>
             <h2>
-                What's New in v{versionNumber}:
+                What's New in v{latestVersion}:
             </h2>
             <ul>
                 {updates.map(update => <li>- {update}</li>)}
