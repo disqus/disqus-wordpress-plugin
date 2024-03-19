cc50ee58914182af0a175d7883d9d712daffbeb3
Debian <bitnami@ip-172-26-7-254.us-west-2.compute.internal>
2024-03-19 19:04:39 +0000

    add hide and show button for whatsnew

--- frontend/src/ts/components/WhatsNew.tsx
+++ frontend/src/ts/components/WhatsNew.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-const latestVersion = '3.0.25';
-const updates = [
+const latestVersion: String = '3.0.25';
+const updates: Array<String> = [
     'The Disqus Embed will now render properly on sites using WordPress Block Themes in addition to Classic Themes',
     'A refreshed UI for the Disqus plugin admin panel',
     'Robust improvements to manual and automatic syncing: multiple bug fixes and increased allowable manual syncing data range',
@@ -9,15 +9,20 @@ const updates = [
 ]
 
 const WhatsNew: React.FC = () => {
+    const [showWhatsNew, setShowWhatsNew] = React.useState<boolean>(false);
+
+    const handleClick = (): void => {
+        setShowWhatsNew(!showWhatsNew);
+    }
     return (
-        <div className={'whats-new'}>
-            <h2>
-                What's New in v{latestVersion}:
-            </h2>
-            <ul>
-                {updates.map((update, index) => <li key={index}>- {update}</li>)}
-            </ul>
-        </div>
+        <React.Fragment>
+            <button className='button' onClick={handleClick}>{showWhatsNew ? 'Hide' : `What's New in v${latestVersion}?`}</button>
+            <div className={`whats-new${showWhatsNew ? '' : ' hidden'}`}>
+                <ul>
+                    {updates.map((update, index) => <li key={index}>- {update}</li>)}
+                </ul>
+            </div>
+        </React.Fragment>
     );
 };
 
