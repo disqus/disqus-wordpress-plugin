32858f11f893962c53774aa152a09ab74195fc30
Debian <bitnami@ip-172-26-7-254.us-west-2.compute.internal>
2024-03-19 18:27:03 +0000

    add ad-optional copy to support tab

--- frontend/src/ts/components/HelpResources.tsx
+++ frontend/src/ts/components/HelpResources.tsx
@@ -3,6 +3,9 @@ import * as React from 'react';
 const HelpResources = () => (
     <ul className='description'>
         <li>
+            <p>In addition to our free-to-use, ad-supported Basic plan, we also offer ad-optional subscription plans that come with more advanced features and access to priority support. Please see our <a href='' target='_blank'>pricing page</a> for more details.</p>
+        </li>
+        <li>
             <a href='https://status.disqus.com/' target='_blank'>
                 {__('Disqus System Status')}
             </a>
