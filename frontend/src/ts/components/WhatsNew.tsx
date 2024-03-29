2932e0f704b043b3ec2a7bc799ea3898a37e76ca
Chris Tang <82986336+chrisjtang@users.noreply.github.com>
2024-03-28 22:10:26 -0700

    UI/UX Refresh (#134)
    * add WhatsNew component
    
    * add more to WhatsNew
    
    * add keys to WhatsNew
    
    * welcome panel link styling update
    
    * update welcome panel banner text, styling, and spacing.  also add mobile styling and centering
    
    * hover effects and styling for nav tabs
    
    * border-radius for welcome panel
    
    * welcome-panel spacing
    
    * add scroll for nav-tab overflow for mobile
    
    * add button hover and focus effects
    
    * link styling across plugin
    
    * add icon hover effects
    
    * decrease bottom-border for support icon hover to 1px
    
    * add border radius to input fields
    
    * add ad-optional copy to support tab
    
    * add hide and show button for whatsnew
    
    * remove box shadow on focus for disqus logo
    
    * add styling for whats-new container
    
    * update readme
    
    * update link in help resources for pricing page
    
    * add remove ads button
    
    * scope all changes to #disqus-admin id
    
    * update latest version to 3.1
    
    * linting
    
    * fix snapshot tests
    
    * update spacing for link in support
    
    ---------
    
    Co-authored-by: Chris <ctang@zetaglobal.com>

---
+++ frontend/src/ts/components/WhatsNew.tsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+
+const latestVersion: String = '3.1';
+const updates: Array<String> = [
+    'The Disqus Embed will now render properly on sites using WordPress Block Themes in addition to Classic Themes',
+    'A refreshed UI for the Disqus plugin admin panel',
+    'Robust improvements to manual and automatic syncing: multiple bug fixes and increased allowable manual syncing data range',
+    'More informative error messaging in the browser console and plugin UI when encountering issues with manual and automatic syncing for easier troubleshooting'
+]
+
+const WhatsNew: React.FC = () => {
+    const [showWhatsNew, setShowWhatsNew] = React.useState<boolean>(false);
+
+    const handleClick = (): void => {
+        setShowWhatsNew(!showWhatsNew);
+    }
+    return (
+        <div className={`whats-new${showWhatsNew ? ' visible' : ''}`}>
+            <button className='button' onClick={handleClick}>{showWhatsNew ? 'Hide' : `What's New in v${latestVersion}?`}</button>
+            <div className={`${showWhatsNew ? '' : ' hidden'}`}>
+                <ul>
+                    {updates.map((update, index) => <li key={index}>- {update}</li>)}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default WhatsNew;
