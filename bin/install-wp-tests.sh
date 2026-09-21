869cc8db02ebb24ebb7494b5cff6af650e465e12
Chris Tang <ctang@zetaglobal.com>
2026-09-21 13:57:08 -0700

    Use MySQL 8 and wait for a healthy database in Docker
    MySQL 5.7 has no ARM image, and newer mysqladmin clients fail SSL checks
    in the test container.

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -144,7 +144,7 @@ install_db() {
 	fi
 
 	# create database
-	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA
+	mysqladmin create $DB_NAME --user="$DB_USER" --password="$DB_PASS"$EXTRA --skip-ssl
 }
 
 install_wp
